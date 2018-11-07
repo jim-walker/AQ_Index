7f8e788ce7312df16abd930318772dcdd228628a
Genie Yang <genieyang93@gmail.com>
2018-11-06 19:24:17 -0500

    Query API

--- api.js
+++ api.js
@@ -1,1 +1,6 @@
 //API JS FILE
+
+//Build the query URL for the ajax request to the  Rest Country API
+const queryURL = `https://restcountries.eu/rest/v2/all`
+
+//Build the query URL for the ajax request to the  Open AQ API
